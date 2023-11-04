e1a9b74eadfb5e6d9081cf6d9060a01b95226f84
Raleigh Norris <norrisrp@vcu.edu>
2023-11-04 17:28:36 -0400

    All Together Now

--- test1.java
+++ test1.java
@@ -1,11 +1,11 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-class test2 {
+class test1 {
 
 	@Test
 	void test() {
-		Assert.assertTrue(0 == Math.MathOperate(7,2,'%'));
+		Assert.assertTrue(14 == Math.MathOperate(7,2,'*'));
 	}
 
 }
